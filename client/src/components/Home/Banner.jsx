61f3322799a8bfc5575cfafec99182b1e7b5981b
Karthik P <padiyil2004@gmail.com>
2025-10-22 23:12:50 +0530

    frontend of banner and hero section

---
+++ client/src/components/Home/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Banner = () =>  {
+    return (
+        <div className="w-full py-1.5 font-medium text-sm text-white text-center bg-gradient-to-r from-[#e62525] to-[#f7e1dd]">
+            <p><span className="inline-block px-3 py-1 rounded-lg text-white bg-red-600 mr-2 transform hover:scale-105 transition-all duration-500 cursor-pointer">New</span>AI Resume Builder</p>
+        </div>
+    );
+};
+
+export default Banner
+  
---
+++ client/src/components/Home/Hero.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Hero = () => {
+  const navigate = useNavigate();
+  return (
+    <div className='flex flex-col min-h-[80vh]'>
+      <div className='flex justify-between items-center px-24 py-6'>
+        <p className='text-4xl font-semibold cursor-pointer'>resume <span className='inline-block w-3 h-3 bg-red-500 rounded-full cursor-pointer'/></p>
+        <div className='flex gap-8 text-1xl ml-18 transition duration-500'>
+          <p className='hover:text-red-500 cursor-pointer transition'>Home</p>
+          <p className='hover:text-red-500 cursor-pointer transition'>Features</p>
+          <p className='hover:text-red-500 cursor-pointer transition'>contact</p>
+        </div>
+        <div className='flex gap-5 '>
+          <button className='bg-red-500 text-white rounded-4xl px-5 py-2 cursor-pointer hover:scale-105 active:scale-90 transition-all duration-500'>Get Started</button>
+          <button className='border rounded-4xl px-6 py-2 cursor-pointer hover:scale-105 active:scale-90 transition-all duration-500'>Login</button>
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col items-center ml-10 mr-10 justify-center text-center relative">
+        <div className="ml-[420px] absolute w-[550px] h-[605px] inset-0 bg-red-400 blur-[100px] opacity-30 -z-10"></div>
+
+        <h1 className='text-6xl font-bold max-w-3xl'>
+          Land Your dream job with <span className='bg-gradient-to-r from-red-600 to-red-500 bg-clip-text text-transparent'>AI-powered</span> resumes.
+        </h1>
+        <p className='mt-6 text-[19px] max-w-md'>
+          Create, edit and download professional resumes with AI-powered assistance
+        </p>
+        <button className='bg-red-500 text-white px-6 py-2 rounded-4xl hover:scale-105 transition-all duration-500 active:scale-90 mt-5 cursor-pointer'>Get Started ➜</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Hero
