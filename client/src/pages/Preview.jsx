ada39c16b41179acfe56d86165e17d0b5460dfa0
Karthik P <padiyil2004@gmail.com>
2025-11-01 19:08:41 +0530

    Added tthe loader

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets';
+import { Loader } from 'lucide-react';
 
 const Preview = () => {
-  return (
-    <div>
-      <h1>hei pussy</h1>
+  const {resumeId} = useParams();
+  const [resumeData,setResumeData] = useState(null);
+  const loadResumeData = async() => {
+    setResumeData(dummyResumeData.find(resume => resume._id === resumeId || null))
+  }
+  useEffect(()=> {
+    loadResumeData();
+  },[])
+  return resumeData ?  (
+    <div className='bg-slate-100'>
+      
     </div>
+  ) : (
+    <>
+      <Loader />
+    </>
   )
 }
 
