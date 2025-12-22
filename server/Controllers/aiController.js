6604f9c72e44ca6a4dab3a740ba8e5568928d135
Karthik P <padiyil2004@gmail.com>
2025-12-22 10:42:01 +0530

    made ai controller and aiRoutes

--- server/Controllers/aiController.js
+++ server/Controllers/aiController.js
@@ -1,3 +1,4 @@
+import Resume from "../models/resumeModel.js";
 
 
 export const enhanceProfessionalSummary = async(req,res) => {
@@ -49,21 +50,69 @@ export const enhanceJobDescription = async(req,res) => {
 export const uploadResume = async(req,res) => {
     try {
       const {resumeText,title} = req.body;
+      const userId = req.userId;
       if(!resumeText || !title){
         return res.status(400).json({message: "Resume text and title are required"});
       }
+      const systemPrompt = "You are an expert AI agent to extract data from resume";
+      const userPrompt = `extract data from this resume: ${resumeText}
+      
+      Provided data in the following JSON format with no additional text before or after:
+      {
+        professional_summary: { type: String, default: '' },
+        skills: [{ type: String }],
+        personal_info: {
+            image: { type: String, default: '' },
+            full_name: { type: String, default: '' },
+            profession: { type: String, default: '' },
+            email: { type: String, default: '' },
+            phone: { type: String, default: '' },
+            location: { type: String, default: '' },
+            linkedin: { type: String, default: '' },
+            website: { type: String, default: '' }, 
+        },
+        experience: [
+            {
+                company: { type: String },
+                position: { type: String },
+                start_date: { type: String },
+                end_date: { type: String },
+                description: { type: String },
+                is_current: { type: Boolean },
+            }
+        ],
+        project: [
+            {
+                name: { type: String },
+                type: { type: String },
+                description: { type: String },
+            }
+        ],
+        education: [
+            {
+                institution: { type: String },
+                degree: { type: String },
+                field: { type: String },
+                graduation_date: { type: String },
+                gpa: { type: Boolean },
+            }
+        ]
+      }`;
       const response = await openai.chat.completions.create({
           model: process.env.OPENAI_MODEL,
           messages: [
-              { role: "system", content: "You are an expert in resume writing. Your task is to enhance the job description of a resume. The job description should be only in 1-2 sentence also highlighting key responsibilities and achievements. Use action verbs and quantifiable results where possible. Make it ATS-friendly. and only return text no options or anything else." },
+              { role: "system", content: systemPrompt },
               {
                   role: "user",
-                  content: userContent,
+                  content: userPrompt,
               },
           ],
+          response_format:{type: 'json_object'}
       });
-      const enhancedContent = response.choices[0].message.content;
-      return res.status(200).json({enhancedContent});
+      const extractedData = response.choices[0].message.content;
+      const parsedData = JSON.parse(extractedData);
+      const newResume = await Resume.create({userId,title,...parsedData})
+      return res.status(200).json({resumeId:newResume._id});
     } catch (error) {
       res.status(500).json({message: "Internal server error"});
     }
