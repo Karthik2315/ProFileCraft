0b86d84566676612fb98af13fa93fac41765c9e8
Karthik P <padiyil2004@gmail.com>
2025-12-22 19:24:54 +0530

    updated Hero section for showing Dashboard button when user is logged in

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import {useSelector} from 'react-redux';
 
 const Hero = () => {
+  const {user} = useSelector(state => state.auth);
   const navigate = useNavigate();
   return (
     <div className='flex flex-col min-h-[80vh]'>
@@ -13,12 +15,13 @@ const Hero = () => {
           <p className='hover:text-red-500 cursor-pointer transition'>contact</p>
         </div>
         <div className='flex gap-5 '>
-          <button className='bg-red-500 text-white rounded-4xl px-7 py-2 cursor-pointer hover:scale-105 active:scale-90 transition-all duration-500' onClick={() => navigate('/app')}>Get Started</button>
+          <button className='bg-red-500 text-white rounded-4xl px-7 py-2 cursor-pointer hover:scale-105 active:scale-90 transition-all duration-500' onClick={() => navigate('/app')} hidden={user}>Get Started</button>
           <button className='border rounded-4xl px-6 py-2 cursor-pointer hover:scale-105 active:scale-90 transition-all duration-500' onClick={()=>{
             setTimeout(() => {
               navigate('/login')
             },700)
-          }}>Login</button>
+          }} hidden={user}>Login</button>
+          <button className='flex-1 bg-red-500 text-white rounded-4xl px-7 py-2 cursor-pointer hover:scale-105 active:scale-90 transition-all duration-500 ml-11' onClick={() => navigate('/app')} hidden={!user}>Dashboard</button>
         </div>
       </div>
       <div className="flex-1 flex flex-col items-center ml-10 mr-10 justify-center text-center relative">
